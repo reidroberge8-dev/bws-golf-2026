e40c5b90e49eb2a4abbb72922d8ba4235297f017
Reid Roberge <reroberg@amazon.com>
2026-08-19 21:03:47 -0400

    Rename Breakfast & Departure Times label to Departure Time

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BW&S 2026 — Service Worker v3: always-fresh navigation */
-const CACHE = 'bws-2026-v7';
+const CACHE = 'bws-2026-v8';
 
 self.addEventListener('install', () => self.skipWaiting());
 
